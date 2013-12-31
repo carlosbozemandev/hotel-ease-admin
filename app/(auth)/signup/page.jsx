3f762b3404cdd16347a1c63ef029c2dcd6c9f057
Carlos <carlosbozemandev@gmail.com>
2014-01-01 00:00:00 +0500

    login and signup page done

--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -3,21 +3,29 @@ import React from "react";
 import { useForm, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-import { FaUser, FaLock } from "react-icons/fa";
+import { FaUser, FaLock, FaEnvelope } from "react-icons/fa";
 import Link from "next/link";
 
 const schema = yup.object().shape({
   email: yup.string().email("Invalid email").required("Email is required"),
   password: yup.string().required("Password is required"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Passwords must match")
+    .required("Confirm Password is required"),
 });
 
-const LoginPage = () => {
-  const { control, handleSubmit, formState: { errors } } = useForm({
+const SignupPage = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     resolver: yupResolver(schema),
   });
 
   const onSubmit = (data) => {
-    // Handle login logic here
+    // Handle signup logic here
     console.log(data);
   };
 
@@ -25,7 +33,7 @@ const LoginPage = () => {
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded shadow-md w-96">
         <h2 className="text-3xl font-bold mb-6 text-center text-blue-600">
-          Welcome Back!
+          Create an Account
         </h2>
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-6">
@@ -72,19 +80,40 @@ const LoginPage = () => {
               <p className="text-red-500 text-sm mt-1">{errors.password.message}</p>
             )}
           </div>
-          <div className="flex my-2 justify-end"><Link href={'/forgot-password'}>Forgot Password</Link></div>
+          <div className="mb-6">
+            <div className="flex items-center mb-2">
+              <label className="block text-sm text-gray-600 mr-3">
+                <FaLock />
+              </label>
+              <Controller
+                name="confirmPassword"
+                control={control}
+                render={({ field }) => (
+                  <input
+                    {...field}
+                    type="password"
+                    placeholder="Confirm Password"
+                    className={`w-full py-2 border-b focus:outline-none focus:border-blue-500 ${errors.confirmPassword ? 'border-red-500' : 'border-gray-300'}`}
+                  />
+                )}
+              />
+            </div>
+            {errors.confirmPassword && (
+              <p className="text-red-500 text-sm mt-1">{errors.confirmPassword.message}</p>
+            )}
+          </div>
           <button
             type="submit"
             className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
           >
-            Login
+            Sign Up
           </button>
         </form>
         <div className="mt-4 text-center">
           <p className="text-gray-600">
-            Don't have an account?{" "}
-            <Link href="/signup" className="text-blue-500 hover:underline">
-              Sign up here
+            Already have an account?{" "}
+            <Link href="/" className="text-blue-500 hover:underline">
+              Log in here
             </Link>
           </p>
         </div>
@@ -93,4 +122,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default SignupPage;
