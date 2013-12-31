d929c127ffa26b089ca2d263df5a833eab708def
Carlos <carlosbozemandev@gmail.com>
2014-01-01 00:00:00 +0500

    forgot password done

--- app/(auth)/forgot-password/page.jsx
+++ app/(auth)/forgot-password/page.jsx
@@ -1,7 +1,76 @@
-import React from 'react'
+'use client'
+import React from "react";
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { FaEnvelope } from "react-icons/fa";
+import Link from "next/link";
 
-export default function page() {
+const schema = yup.object().shape({
+  email: yup.string().email("Invalid email").required("Email is required"),
+});
+
+const ForgotPasswordPage = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data) => {
+    // Handle forgot password logic here
+    console.log(data);
+  };
+
   return (
-    <div>page</div>
-  )
-}
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded shadow-md w-96">
+        <h2 className="text-3xl font-bold mb-6 text-center text-blue-600">
+          Forgot Password
+        </h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-6">
+            <div className="flex items-center mb-2">
+              <label className="block text-sm text-gray-600 mr-3">
+                <FaEnvelope />
+              </label>
+              <Controller
+                name="email"
+                control={control}
+                render={({ field }) => (
+                  <input
+                    {...field}
+                    type="email"
+                    placeholder="Email"
+                    className={`w-full py-2 border-b focus:outline-none focus:border-blue-500 ${errors.email ? 'border-red-500' : 'border-gray-300'}`}
+                  />
+                )}
+              />
+            </div>
+            {errors.email && (
+              <p className="text-red-500 text-sm mt-1">{errors.email.message}</p>
+            )}
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+          >
+            Reset Password
+          </button>
+        </form>
+        <div className="mt-4 text-center">
+          <p className="text-gray-600">
+            Remember your password?{" "}
+            <Link href="/" className="text-blue-500 hover:underline">
+              Log in here
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ForgotPasswordPage;
