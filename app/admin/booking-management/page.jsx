d974ddde70ade1a82280e5128f0978408d5dc205
Carlos <carlosbozemandev@gmail.com>
2014-01-01 00:00:00 +0500

    graph colors update

--- app/admin/booking-management/page.jsx
+++ app/admin/booking-management/page.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div className='bg-black m-5 rounded-lg p-5'>
+    <div className='bg-gray-900 m-5 rounded-lg p-5'>
     <h1 className='text-4xl text-white mb-5 font-semibold'>Bookings</h1>
       <StackedLineChart />
     </div>
--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -6,11 +6,11 @@ import React from 'react'
 export default function page() {
   return (
     <div>
-      <div className='bg-black m-5 rounded-lg p-5'>
+      <div className='bg-gray-900 m-5 rounded-lg p-5'>
         <h1 className='text-4xl text-white mb-5 font-semibold'>Bookings</h1>
       <SameDataComposedChart />
       </div>
-      <div className='bg-black m-5 rounded-lg p-5'>
+      <div className='bg-gray-900 m-5 rounded-lg p-5'>
         <h1 className='text-4xl text-white mb-5 font-semibold'>User And Mangers</h1>
       <StackedAreaChart />
       </div>
